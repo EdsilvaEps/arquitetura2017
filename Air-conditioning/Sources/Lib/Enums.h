ff7ac11f57b79225d22dfd65dfcf99dab54eacad
Joao Danilo <joaodanilo1992@gmail.com>
2017-08-23 10:20:56 -0400

    Update Enums.h

--- Air-conditioning/Sources/Lib/Enums.h
+++ Air-conditioning/Sources/Lib/Enums.h
@@ -1,8 +1,19 @@
 #ifndef SOURCES_ENUMS_H_
 #define SOURCES_ENUMS_H_
 
+/**
+ * Operation mode of GPIO port
+ */
 enum {INPUT = 0, OUTPUT};
+
+/**
+ * State of GPIO port
+ */
 enum {LOW = 0, HIGH};
-//enum {A0 = 8, A1 = 9, A2 = 12, A3 = 13, A4 = 11, A5 = 15};
+
+
+/**
+ * The physical pins of the board MKL25Z (identical to the Arduino)
+ */
 enum {D0 = 0, D1, D2 ,D3 ,D4 ,D5 ,D6 ,D7 ,D8 ,D9 ,D10 ,D11 ,D12 ,D13};
 #endif
