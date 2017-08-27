adf80b0bd021c7c27c8470e1c4d913d26a3ad04e
Joao Danilo <joaodanilo1992@gmail.com>
2017-08-27 12:17:06 -0400

    Lógica do multiplexador até o display implementada

--- Ar-condicionado/Sources/main.cpp
+++ Ar-condicionado/Sources/main.cpp
@@ -1,27 +1,44 @@
 #include <MKL25Z4.h>
 #include <Lib/PIT/PITPeriodicInterrupt/mkl_PITPeriodicInterrupt.h>
 #include <Lib/Display/dsf_SerialDisplays.h>
+#include <Lib/GPIO/mkl_GPIOPort.h>
+#include <Lib/Multiplexer/Multiplexer.h>
+#include <Lib/TPM/TPM_Delay/mkl_TPMDelay.h>
 
-dsf_SerialDisplays display(gpio_PTC7, gpio_PTC0, gpio_PTC3);
-mkl_PITInterruptInterrupt pit(PIT_Ch0);
+dsf_SerialDisplays display(gpio_PTB2, gpio_PTB1, gpio_PTA12);
 
-extern "C" {
-  void PIT_IRQHandler(void){
-    disp.updateDisplays();
-  }
-}
+mkl_GPIOPort onoffButton(gpio_PTB10);
+
+Multiplexer multiplex;
+mkl_TPMDelay delay(tpm_TPM0);
+
+uint32_t debounceTime = 24576;
+
+bool flagTest = false;
+uint8_t temperatura = 45;
+uint8_t contador = 19;
 
 int main(void){
 
-  pit.enablePeripheralModule();
-  pit.setPeriod(0x4e20);
-  pit.resetCounter();
-  pit.enableTimer();
-  pit.enableInterruptRequests();
   display.clearDisplays();
-  display.writeWord(2222);
+
+  onoffButton.setPortMode(gpio_input);
+  onoffButton.setPullResistor(gpio_pullUpResistor);
+
+  delay.setFrequency(tpm_div128);
+  delay.startDelay(10);
 
   while(true){
+
+    uint8_t resultMultiplex = multiplex.select(flagTest,contador, temperatura);
+
+    display.writeWord(resultMultiplex,flagTest+1);
+
+    if(!onoffButton.readBit() && delay.timeoutDelay()){
+
+      flagTest = !flagTest;
+      delay.startDelay(debounceTime);
+    }
 
   }
 
