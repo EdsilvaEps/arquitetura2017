a4b00b9b93dcfd80da11a5d3bf561ed2a802f75f
Joao Danilo <joaodanilo1992@gmail.com>
2017-08-27 12:15:36 -0400

    Lógica do multiplexador até o display implementada

--- Ar-condicionado/Sources/main.cpp
+++ Ar-condicionado/Sources/main.cpp
@@ -1,12 +1,30 @@
-#include "MKL25Z4.h"
+#include <MKL25Z4.h>
+#include <Lib/PIT/PITPeriodicInterrupt/mkl_PITPeriodicInterrupt.h>
+#include <Lib/Display/dsf_SerialDisplays.h>
+
+dsf_SerialDisplays display(gpio_PTC7, gpio_PTC0, gpio_PTC3);
+mkl_PITInterruptInterrupt pit(PIT_Ch0);
+
+extern "C" {
+  void PIT_IRQHandler(void){
+    disp.updateDisplays();
+  }
+}
 
 int main(void){
 
+  pit.enablePeripheralModule();
+  pit.setPeriod(0x4e20);
+  pit.resetCounter();
+  pit.enableTimer();
+  pit.enableInterruptRequests();
+  display.clearDisplays();
+  display.writeWord(2222);
 
-    while(true){
+  while(true){
 
-    }
+  }
 
-    return 0;
+  return 0;
 }
 
