01b019693751af6f0135ed735f19e05938f826de
Rebeca Nunes Rodrigues <rebeca.n.rod@gmail.com>
2017-08-31 04:14:27 -0400

    Adicionar metodo reset

--- Air-conditioning/Sources/Lib/FrequencyDivider/FrequencyDivider.h
+++ Air-conditioning/Sources/Lib/FrequencyDivider/FrequencyDivider.h
@@ -1,51 +1,56 @@
-#ifndef SOURCES_FREQUENCYDIVIDER_H_
-#define SOURCES_FREQUENCYDIVIDER_H_
+#ifndef FREQUENCYDIVIDER_H_
+#define FREQUENCYDIVIDER_H_
 
 #include <stdint.h>
 
 /**
  * Class FrequencyDivider - Represent a frequency divider device
  * @author Salmiakki Team
- * @version 1.0, 2017-08-23
+ * @version 2.0, 2017-08-30
  */
-class FrequencyDivider{
 
-  public:
+ class FrequencyDivider{
+	public:
+	 	/**
+	 	 * Constructor class
+	 	 * @param input - Clock with original frequency
+	 	 * @param reset - Reset the internal counter
+	 	 * @param divider - Number of times the clock will be divided
+	 	 */
+	 	FrequencyDivider(uint32_t divider);
 
-    /**
-     * Constructor class - Enter with the number of times the clock will be divided
-     * @param divider Clock divider
-     */
-    FrequencyDivider(uint32_t divider);
+	 	/**
+	 	 * Reset counter
+	 	 */
+	 	void reset();
+	 	/**
+	 	 * Calculates the clock division
+	 	 */
+	 	void clockDiv();
 
-    /**
-     * Calculates the clock division
-     */
-    void clockDiv();
+	 	/**
+	 	 * Take the current state of the flag
+	 	 * @return State of flag (TRUE or FALSE)
+	 	 */
+	 	uint8_t getFlag();
 
-    /**
-     * Take the current state of the flag
-     * @return State of flag (TRUE or FALSE)
-     */
-    uint8_t getFlag();
+	 	/**
+	 	 * Informs if the flag has been detected
+	 	 * @param flag
+	 	 */
+	 	void setFlag();
 
-    /**
-     * Informs if the flag has been detected
-     * @param flag
-     */
-    void setFlag();
+	private:
 
-  private:
+		/** Number of times the clock is being divided */
+		uint32_t _divider;
 
-    /** Number of times the clock is being divided */
-    uint32_t _divider;
+		/** Aux counter */
+		uint32_t _counter = 1;
 
-    /** Aux counter */
-    uint32_t _counter = 1;
+		/** Flag that informs if the counter is equal to the divisor */
+		uint8_t _flag = false;
 
-    /** Flag that informs if the counter is equal to the divisor */
-    uint8_t _flag = false;
-
-};
+ };
 
 #endif
